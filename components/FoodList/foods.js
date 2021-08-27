b48bea9d171a35deafd7501809e36d4e109e0876
ayamohamed99 <aya199taha@gmail.com>
2021-08-28 01:23:05 +0200

    screens

--- components/FoodList/foods.js
+++ components/FoodList/foods.js
@@ -1,4 +1,10 @@
-export default [{
+export default [
+  {
+    name: 'Ice Cream',
+    tagline: 'You’ve got ice cream,',
+    taglineCTA: 'now what?',
+    image: require('../../assets/images/icecream.jpeg'),
+  },{
   name: 'Burger',
   tagline: 'burger without cheese is like ',
   taglineCTA: '\n    a hug without a squeeze    ',
@@ -18,4 +24,30 @@ export default [{
   tagline: 'The first wealth is ',
   taglineCTA: 'healt',
   image: require('../../assets/images/Healthy.jpeg'),
-}];
+},
+
+{
+  name: 'Cookies',
+  tagline: 'C is for cookie',
+  taglineCTA: 'That’s good enough for me.',
+  image: require('../../assets/images/cookies.jpeg'),
+},
+{
+  name: 'salad',
+  tagline: 'Chock full of delicious ',
+  taglineCTA: 'flavors!',
+  image: require('../../assets/images/salad.jpeg'),
+},
+{
+  name: 'Coffee',
+  tagline: 'Humanity runs on',
+  taglineCTA: 'coffee.',
+  image: require('../../assets/images/cofe.jpeg'),
+},
+{
+  name: 'Juice ',
+  tagline: 'The darker the berry,',
+  taglineCTA: 'the sweeter the juice.',
+  image: require('../../assets/images/drink.jpeg'),
+}
+];
